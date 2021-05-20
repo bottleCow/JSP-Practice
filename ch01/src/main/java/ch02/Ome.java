37ae833764f236a5603d4f2f9cd1ef218a95301c
뱅우 <@192.168.2.2>
2021-05-20 14:36:13 +0900

    132r3f12f312

---
+++ ch01/src/main/java/ch02/Ome.java
@@ -0,0 +1,32 @@
+package ch02;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Ome")
+public class Ome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] menu = request.getParameterValues("menu");
+		//여러개(배열)은 getParameterValues()
+		//한개는 getParameter()
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h2>오늘의 메뉴</h2>");
+		for(int i = 0; i < menu.length; i++) {
+			if (i == menu.length - 1)
+				out.println(menu[i]); // 마지막 데이터 이면,생략
+			else out.println(menu[i]+", ");
+		}
+		out.println("</body></html>");
+		out.close();
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		doGet(request, response);
+	}
+}
